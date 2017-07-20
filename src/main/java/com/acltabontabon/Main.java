4049b41f152ecccbdd0a1693e6b0e32d522492e3
Alvin Cris Tabontabon <acltabontabon@gmail.com>
2017-07-21 00:14:57 +0800

    Intentional error

--- src/main/java/com/acltabontabon/Main.java
+++ src/main/java/com/acltabontabon/Main.java
@@ -3,6 +3,6 @@ package com.acltabontabon;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello Jenkins!")
     }
 }
