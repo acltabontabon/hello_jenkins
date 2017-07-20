d93e3f47198c5345e228213152203f7a79f69605
Alvin Cris Tabontabon <alvincristabontabon@Alvins-MacBook-Pro.local>
2017-07-20 22:45:19 +0800

    Initial commit

---
+++ src/main/java/com/acltabontabon/Main.java
@@ -0,0 +1,11 @@
+package com.acltabontabon;
+
+/**
+ * Created by alvincristabontabon on 20/07/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+    }
+}
